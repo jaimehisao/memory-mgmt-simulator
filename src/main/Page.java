6f44e1a6c774ed33ce8c779085e23f434467d6f3
AntonyTC <49083894+A01561769@users.noreply.github.com>
2020-08-01 14:46:16 -0600

    Update Page.java
    Input from files added

--- src/main/Page.java
+++ src/main/Page.java
@@ -1,81 +1,98 @@
 package main;
 
-public class Page {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-    int num;
-    Process process;
-    int locationInMemory;
-    int locationInSwap;
-    int lastAppearance;
-    int timeInserted;
-    int timesUsed;
-    boolean inserted;
-
-     public Page(int num, Process process){
-        this.num = num;
-        this.locationInMemory = 0;
-        this.locationInSwap = 0;
-        this.inserted = true;
-        this.lastAppearance = -1;
-        this.timesUsed = 0;
-        this.process = process;
+public class Main {
+    
+    static Scanner scanner = new Scanner(System.in);
+    static Scanner fScanner;
+    
+    public static void main(String args[]){
+        System.out.println("Welcome to the memory management system! \n ");
+        
+        try{
+            // Get file path, open it and scan it
+            String sPath;
+            System.out.println("Please write the file path you want to use");
+            sPath = scanner.nextLine();
+            File myFile = new File(sPath);
+            fScanner = new Scanner(myFile);
+            System.out.println("Thank you!");
+            
+            int option = 99;
+            
+            while(option != 0 && option != 1){
+                System.out.println("Please choose the memory replacement policy you want to use...\n" +
+                        "1.- Least Recently Used\n" +
+                        "2.- First In First Out");
+                option = scanner.nextInt();
+            }
+            
+            Simulation simulation;
+            
+            if(option == 0){
+                simulation = new Simulation(0); //Type FIFO
+                System.out.println("Created simulation using FIFO replacement policy.");
+            }else {
+                simulation = new Simulation(1); //Type LRU
+                System.out.println("Created simulation using LRU replacement policy.");
+            }
+            
+            
+            while(fScanner.hasNextLine()){
+                
+                // Get and split line
+                String[] input = fScanner.nextLine().split("\\W+");
+                
+                if("P".equals(input[0])){
+                    //Assign X amount of bytes to process Z
+                    System.out.println("Syntax: Process Size and PID (534 5838 assigns 534 bytes to PID 5838)");
+                    int size = Integer.parseInt(input[1]);
+                    int processID = Integer.parseInt(input[2]);
+                    // Test Input
+                    System.out.println("INPUT:\n" + input[0] + " " + size + " " + processID);
+                    simulation.createNewProcess(processID, size);
+                }
+                
+                if("A".equals(input[0])){
+                    //Obtain physical address that corresponds to X virtual address of process Z
+                    System.out.println("Syntax: Virtual Address, Process ID, M\n" +
+                            "Where if M is 0, only the address is returned, if it is 1 it also modifies the address.");
+                    //TODO add 0/1 option
+                    int addr = Integer.parseInt(input[1]);
+                    int processID = Integer.parseInt(input[2]);
+                    String modify = input[3];
+                    // Test Input
+                    System.out.println("INPUT:\n" + input[0] + " " + addr + " " + processID + " " + modify);
+                    
+                    System.out.println("The physical address for virtual address " + addr + " from process " + processID +
+                            " is " + simulation.returnPhysicalAddress(addr, processID, Boolean.parseBoolean(modify)));
+                }
+                
+                if("L".equals(input[0])){
+                    //Free page frames occupied by process X
+                    int processID = Integer.parseInt(input[1]);
+                    // Test Input
+                    System.out.println("INPUT:\n" + input[0] + " " + processID);
+                    
+                    System.out.println("Deleted " + simulation.freeProcessFromMemory(processID) + " frames from PID " + processID);
+                }
+                
+                if("F".equals(input[0])){
+                    // Test Input
+                    System.out.println("INPUT:\n" + input[0]);
+                    
+                    return;
+                }
+                simulation.viewSimulation(); //this is debug output, not prod level
+            }
+        } catch(FileNotFoundException | NumberFormatException e){}
+        
+        
+        
     }
-
-    public void setLocationInSwap(int locationInSwap) {
-        this.locationInSwap = locationInSwap;
-    }
-
-    public int getTimeInserted() {
-        return timeInserted;
-    }
-
-    public void setTimeInserted(int timeInserted) {
-        this.timeInserted = timeInserted;
-    }
-
-    public int getLocationInMemory() {
-        return locationInMemory;
-    }
-
-    public void setLocationInMemory(int locationInMemory) {
-        this.locationInMemory = locationInMemory;
-    }
-
-    public int getLastAppearance() {
-        return lastAppearance;
-    }
-
-    public int getLocationInSwap() {
-        return locationInSwap;
-    }
-
-    public void setInserted(boolean inserted) {
-        this.inserted = inserted;
-    }
-
-    public int getNum() {
-        return num;
-    }
-
-    public Process getProcess(){ return process; }
-
-    public void setLastAppearance(int lastAppearance) {
-        this.lastAppearance = lastAppearance;
-    }
-
-    public int getTimesUsed() {
-        return timesUsed;
-    }
-
-    public void setTimesUsed(int timesUsed) {
-        this.timesUsed = timesUsed;
-    }
-
-    public boolean isInserted() {
-        return inserted;
-    }
-
-    public void setNum(int num) {
-        this.num = num;
-    }
+    
+    
 }
