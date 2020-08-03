0235ac0756c0ba87e92d2a0956563f1ed3fea85e
AntonyTC <49083894+A01561769@users.noreply.github.com>
2020-08-03 14:26:05 -0600

    Added documentation

--- src/main/Commons.java
+++ src/main/Commons.java
@@ -2,6 +2,7 @@ package main;
 
 public class Commons {
 
+    //Size of pages, memory and storage in bits
     public static final int PAGE_SIZE = 16;
     public static final int MEMORY_SIZE = 2048;
     public static final int SWAP_SIZE = 4096;
