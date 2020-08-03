103cb12bb83f050b855ebae33ef91c51c280fed3
AntonyTC <49083894+A01561769@users.noreply.github.com>
2020-08-03 13:28:50 -0600

    Added documentation

--- src/main/Page.java
+++ src/main/Page.java
@@ -23,63 +23,121 @@ public class Page {
     }
     
     /**
-     * Setter of the virtual memory location
+     * Set the page's location in storage
      * @param locationInSwap 
      */
     public void setLocationInSwap(int locationInSwap) {
         this.locationInSwap = locationInSwap;
     }
-
+    
+    /**
+     * Get the page's time inserted
+     * @return timeInserted
+     */
     public int getTimeInserted() {
         return timeInserted;
     }
 
+    /**
+     * Set the page's time inserted
+     * @param timeInserted 
+     */
     public void setTimeInserted(int timeInserted) {
         this.timeInserted = timeInserted;
     }
 
+    /**
+     * Get the page's location in memory
+     * @return timeInserted
+     */
     public int getLocationInMemory() {
         return locationInMemory;
     }
 
+    /**
+     * Set the page's location in memory
+     * @param locationInMemory 
+     */
     public void setLocationInMemory(int locationInMemory) {
         this.locationInMemory = locationInMemory;
     }
 
+    /**
+     * Get the page's last appearance
+     * @return lastAppearance
+     */
     public int getLastAppearance() {
         return lastAppearance;
     }
 
+    /**
+     * Get the page's location in storage
+     * @return locationInSwap
+     */
     public int getLocationInSwap() {
         return locationInSwap;
     }
 
+    /**
+     * Set the page's reference bit
+     * @param referenceBit 
+     */
     public void setReferenceBit(boolean referenceBit) {
         this.referenceBit = referenceBit;
     }
 
+    /**
+     * Get the page's number
+     * @return num
+     */
     public int getNum() {
         return num;
     }
 
-    public Process getProcess(){ return process; }
+    /**
+     * Get the page's process
+     * @return process
+     */
+    public Process getProcess(){ 
+        return process; 
+    }
 
+    /**
+     * Set the page's last appearance
+     * @param lastAppearance 
+     */
     public void setLastAppearance(int lastAppearance) {
         this.lastAppearance = lastAppearance;
     }
 
+    /**
+     * Get the page's times used
+     * @return timesUsed
+     */
     public int getTimesUsed() {
         return timesUsed;
     }
 
+    /**
+     * set the page's times used
+     * @param timesUsed 
+     */
     public void setTimesUsed(int timesUsed) {
         this.timesUsed = timesUsed;
     }
 
+    /**
+     * Get the page's reference bit
+     * @return 
+     */
     public boolean isReferenceBit() {
         return referenceBit;
     }
 
+    /**
+     * Set the page's number
+     * @param num 
+     */
     public void setNum(int num) {
         this.num = num;
     }
