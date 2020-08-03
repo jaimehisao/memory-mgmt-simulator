5c3f331c5ef8254557e7ff866df131c6a718019e
AntonyTC <49083894+A01561769@users.noreply.github.com>
2020-08-03 14:23:42 -0600

    Added documentation

--- src/main/Process.java
+++ src/main/Process.java
@@ -11,6 +11,11 @@ public class Process {
     ArrayList<Page> pageIndexInPrimaryMemory;   //Stores the Page objects of where a process is located in Physical Mem
     ArrayList<Page> pageIndexInSwap;            //Stores the Page objects of where a process is located in Swap Memory.
 
+    /**
+     * Process Constructor
+     * @param processId
+     * @param processSize 
+     */
     public Process(int processId, int processSize){
         this.processId = processId;
         this.processSize = processSize;
@@ -19,50 +24,114 @@ public class Process {
         this.timecreated = Main.getSimulation().getSystemTimestamp();
     }
 
-    public void addToSwapPageIndex(Page page){ pageIndexInSwap.add(page); }
+    /**
+     * Add page to swap index
+     * @param page 
+     */
+    public void addToSwapPageIndex(Page page){ 
+        pageIndexInSwap.add(page); 
+    }
 
+    /**
+     * Get Process ID
+     * @return processId
+     */
     public int getProcessId() {
         return processId;
     }
 
-    public void addPageToPrimaryMemory(Page page){ this.pageIndexInPrimaryMemory.add(page);}
+    /**
+     * Add page to memory
+     * @param page 
+     */
+    public void addPageToPrimaryMemory(Page page){ 
+        this.pageIndexInPrimaryMemory.add(page);
+    }
 
-    public void removePageFromPrimaryMemory(Page page){ this.pageIndexInPrimaryMemory.remove(page); }
+    /**
+     * Remove page from memory
+     * @param page 
+     */
+    public void removePageFromPrimaryMemory(Page page){ 
+        this.pageIndexInPrimaryMemory.remove(page); 
+    }
 
+    /**
+     * Get Process size
+     * @return processSize
+     */
     public int getProcessSize() {
         return processSize;
     }
 
+    /**
+     * Set process ID
+     * @param processId 
+     */
     public void setProcessId(int processId) {
         this.processId = processId;
     }
 
+    /**
+     * Set process size
+     * @param processSize 
+     */
     public void setProcessSize(int processSize) {
         this.processSize = processSize;
     }
 
+    /**
+     * Get array of pages
+     * @return pageIndexInPrimaryMemory
+     */
     public ArrayList<Page> getPageIndexInPrimaryMemory() {
         return pageIndexInPrimaryMemory;
     }
 
+    /**
+     * Set PageIndexInPrimaryMemory array
+     * @param pageIndexInPrimaryMemory 
+     */
     public void setPageIndexInPrimaryMemory(ArrayList<Page> pageIndexInPrimaryMemory) {
         this.pageIndexInPrimaryMemory = pageIndexInPrimaryMemory;
     }
 
+    /**
+     * Remove page from memory array
+     * @param index 
+     */
     public void removeProcessPageFromMemory(int index) {
         this.pageIndexInPrimaryMemory.remove(index);
     }
 
-    public void removeFromSwapPageIndex(Page page){ pageIndexInSwap.remove(page); }
+    /**
+     * Remove page from storage array
+     * @param page 
+     */
+    public void removeFromSwapPageIndex(Page page){ 
+        pageIndexInSwap.remove(page); 
+    }
 
+    /**
+     * Get page index in storage 
+     * @return pageIndexInSwap
+     */
     public ArrayList<Page> getPageIndexInSwap() {
         return pageIndexInSwap;
     }
 
+    /**
+     * set page index in storage
+     * @param pageIndexInSwap 
+     */
     public void setPageIndexInSwap(ArrayList<Page> pageIndexInSwap) {
         this.pageIndexInSwap = pageIndexInSwap;
     }
 
+    /**
+     * Get time the process was created
+     * @return timeCreated
+     */
     public int getTimecreated() {
         return timecreated;
     }
